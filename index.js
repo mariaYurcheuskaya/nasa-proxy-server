acf0481a9b558795e08708c45032c8816579efd4
m.yurcheuskaya <m.yurcheuskaya@godeltech.com>
2024-03-08 15:23:02 +0100

    Add env variables and express

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 const axios = require('axios');
 
-const NASA_URL = 'https://api.nasa.gov/neo/rest/v1/feed'
 const START_DATE = '2024-02-26'
 const END_DATE = '2024-03-01'
-const API_KEY = 'Qce19zAbTfBx8PKaGyrZ3ZFc8feagBAjlRBMg3lg'
 
-const buildUrl = (startDate, endDate) => {
-    const myUrlWithParams = new URL(NASA_URL);
-    myUrlWithParams.searchParams.append("start_date", startDate);
-    myUrlWithParams.searchParams.append("end_date", endDate);
-    myUrlWithParams.searchParams.append("api_key", API_KEY);
-    console.log(myUrlWithParams.href);
+
+const printAsteroidsData = (asteroids) => {
+    const data = asteroids.data;
+    console.log(JSON.stringify(data, null, 2));
+    printAsteroidsAmount(data);
 }
 
-const printJson = (data) => {
-    console.log(JSON.parse(data));
-    console.log(data);
+const printAsteroidsAmount = (asteroidsData) => {
+    const amount = asteroidsData.element_count;
+    console.log(`${amount} of asteroids were seen were seen from Monday to Friday`);
 }
 
+const printError = (err) => {
+    err => console.log('Error: ', err.message)
+}
 
 const getAsteroidsAmount = (startDate, endDate) => {
-    axios.get(buildUrl(startDate, endDate))
-        .then(printJson)
-        .catch(err => console.log('Error: ', err.message))
+    axios.get(process.env.NASA_URL, {
+        params: {
+            start_date: startDate,
+            end_date: endDate,
+            api_key: process.env.API_KEY
+        }
+    })
+        .then(printAsteroidsData)
+        .catch(printError)
 }
 
 getAsteroidsAmount(START_DATE, END_DATE);
