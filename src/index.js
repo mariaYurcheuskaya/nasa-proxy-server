e2b8066b95eaf61b7f2ccb4971c868a0954d7d67
mariaYurcheuskaya <151016131+mariaYurcheuskaya@users.noreply.github.com>
2024-03-15 12:25:06 +0100

    Proxy-13: added Sentry (#7)
    * PROXY-13: Adding logging using Sentry
    
    * PROXY-13: Adding logging using Sentry
    
    ---------
    
    Co-authored-by: m.yurcheuskaya <m.yurcheuskaya@godeltech.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
+const Sentry = require('@sentry/node');
+const { nodeProfilingIntegration } = require('@sentry/profiling-node');
 const express = require('express');
 const meteorsRouter = require('./routes/meteors-routers');
 const nasaPhotoRouter = require('./routes/nasa-photo-router');
 const { pageNotFoundHandler, errorHandler } = require('./middlewares');
-const { PORT } = require('./config/environment');
+const { PORT, DSN } = require('./config/environment');
 
 const app = express();
+
+Sentry.init({
+  dsn: DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Sentry.Integrations.Express({ app }),
+    nodeProfilingIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 app.use('/meteors', meteorsRouter);
 app.use('/roverPhoto', nasaPhotoRouter);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(errorHandler);
 app.use('*', pageNotFoundHandler);
